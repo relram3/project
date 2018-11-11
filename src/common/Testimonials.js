d58a6ce34112cd3843ee612c99891d945cfd22d7
Ramzi <ramzi.el12@gmail.com>
2018-11-11 17:20:57 -0600

    Finished the testimonials section

--- src/common/Testimonials.js
+++ src/common/Testimonials.js
@@ -2,7 +2,62 @@ import React, { Component } from "react";
 
 class Testimonials extends Component {
   render() {
-    return <div className="testimonials">TESTIMONIALS</div>;
+    return (
+      <section className="section-stories">
+      <div className="u-center-text u-margin-bottom-medium">
+          <h2 className="heading-secondary">
+          People We’ve Helped
+          </h2>
+      </div>
+
+      <div className="row story-row">
+          <div className="story">
+          <div className="story__shape">
+                <i class="far fa-user-circle icon-large"></i>
+              </div>
+              <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-small">His service was affordable and his client-communication was excellent!</h3>
+                  <p>I hired Bayarjargal for a divorce case in 2015 and again for an immigration case in which my wife sponsored me for green card. He was honest and creative in handling my cases. His service was affordable and his client-communication was excellent!</p>
+              </div>
+          </div>
+      </div>
+      <div className="row story-row">
+          <div className="story">
+              <div className="story__shape">
+                <i class="far fa-user-circle icon-large"></i>
+              </div>
+              <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-small">I would recommend him to anybody in legal trouble or need!</h3>
+                  <p>I was charged with battery/bodily injury with Deadly Weapon. He successfully co-defended me with his partner and the case against me was thrown out in court! He was able clear my record in matter of a couple of months. I now have a clean record and a new job! I would recommend him to anybody in legal trouble or need!</p>
+              </div>
+          </div>
+      </div>
+      <div className="row story-row">
+          <div className="story">
+          <div className="story__shape">
+                <i class="far fa-user-circle icon-large"></i>
+              </div>
+              <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-small">I had a great result with this attorney The affordable legal fee was a huge plus!</h3>
+                  <p>I was charged with one count of battery with bodily injury, a DUI, and 3 other traffic tickets. If convicted, I faced months of jail times plus thousands of fines and other fees. I decided to hire Mr. Sereenen and he did not disappoint me. In fact, after a bench trial, all my charges were dismissed! I had a great result with this attorney. The affordable legal fee was a huge plus!</p>
+              </div>
+          </div>
+      </div>
+      <div className="row story-row">
+          <div className="story">
+          <div className="story__shape">
+                <i class="far fa-user-circle icon-large"></i>
+              </div>
+              <div className="story__text">
+                  <h3 className="heading-tertiary u-margin-bottom-small">I have recommended his services to my friends as well</h3>
+                  <p>Bayarjargal is a very professional and personable attorney. He helped me in forming and registering my business. I was very impressed with his research and evaluating the risk for every option. He was very open and honest on every cost prediction. I have recommended his services to my friends as well. I would definitely keep using his services.</p>
+              </div>
+          </div>
+      </div>
+  </section>
+
+
+    )
   }
 }
 
